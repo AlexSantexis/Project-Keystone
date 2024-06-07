49117f186c5c0b6fdacfca2e8955d63900089a52
Alex S. <alex.santexis@gmail.com>
2024-06-07 13:40:44 +0300

    First Commit

--- Project-Keystone/Infrastructure/Configurations/MapperConfig.cs
+++ Project-Keystone/Infrastructure/Configurations/MapperConfig.cs
@@ -1,9 +1,13 @@
 ﻿using AutoMapper;
+using Project_Keystone.Api.Models.DTOs;
+using Project_Keystone.Core.Entities;
 
 namespace Project_Keystone.Infrastructure.Configurations
 {
     public class MapperConfig : Profile
     {
-        public MapperConfig() { }
+        public MapperConfig() {
+            CreateMap<User,UserDTO>().ReverseMap(); // <from, to>
+        }
     }
 }
